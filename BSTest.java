db9b232ddbdb3ac5f9b9404bd06d947e5d84500d
anonymousdomain <domainanonymous1@gmail.com>
2021-11-08 23:28:01 +0300

    test

--- BSTest.java
+++ BSTest.java
@@ -3,17 +3,19 @@
  */
 public class BSTest {
 
-    public static void main(String[] args) {
-        BST bst=new BST();
-        bst.insert(10);
-        bst.insert(20);
-        bst.insert(15);
-        bst.insert(6);
-        bst.insert(12);
-        bst.visit(bst.root);
-        System.out.println("search");
-System.out.println(bst.search(bst.root, 20).data);
-System.out.println("bfs traversal");
+  public static void main(String[] args) {
+    BST bst = new BST();
+    bst.insert(10);
+    bst.insert(20);
+    bst.insert(15);
+    bst.insert(6);
+    bst.insert(12);
+    bst.visit(bst.root);
+    System.out.println("search");
+    System.out.println(bst.search(bst.root, 20).data);
+    System.out.println("bfs traversal");
     bst.bfs();
-    }
+    System.out.println("dfs");
+    bst.dfs();
+  }
 }
