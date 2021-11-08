f75abe6d2d3d8a2e0ec6a6f6ab0ba0d27efc72cc
anonymousdomain <domainanonymous1@gmail.com>
2021-11-08 22:07:05 +0300

    insertion and search

--- BST.java
+++ BST.java
@@ -3,5 +3,34 @@
  */
 public class BST {
 
+  BSTNode root;
+
+  public BST() {
+    this.root = null;
+  }
+
+  public void visit(BSTNode p) {
+    System.out.println(p.data + " ");
+  }
+
+  public void insert(int e1) {
+    BSTNode p = root, prev = null;
     
+    while (p != null) {
+      prev = p;
+      if (p.data < e1) p = p.right;
+      else  p = p.left;
+    }
+    if (root == null) root = new BSTNode();
+    else if (prev.data < e1) prev.right = new BSTNode(e1); 
+    else prev.left =new BSTNode(e1);
+  }
+  public BSTNode search(BSTNode p,int e1){
+   while (p!=null) {
+     if(p.data==e1)return p;
+     else if(e1<p.data) p=p.left;
+     p=p.right;
+   }
+   return null;
+  }
 }
