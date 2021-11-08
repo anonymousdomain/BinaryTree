cbbbb1c569d1f04c9896476779aa3a470e5a79ea
anonymousdomain <domainanonymous1@gmail.com>
2021-11-08 22:07:35 +0300

    search and insert is tested

---
+++ BSTest.java
@@ -0,0 +1,17 @@
+/**
+ * BSTest
+ */
+public class BSTest {
+
+    public static void main(String[] args) {
+        BST bst=new BST();
+        bst.insert(10);
+        bst.insert(20);
+        bst.insert(15);
+        bst.insert(6);
+        bst.insert(12);
+        bst.visit(bst.root);
+System.out.println(bst.search(
+    bst.root, 20).data);
+    }
+}
